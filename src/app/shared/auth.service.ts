0223dcc7163419639af2111fe26155ff296e3c96
Ivan Sena <ivanprojetosti@gmail.com>
2021-04-01 15:55:34 -0300

    (form + cadastro paciente feito em signup/signup.page.html)

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,15 +1,21 @@
-import { Login } from './../users/shared/login';
-import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import { UsersPaciente } from '../users/shared/users-paciente';
+import { UsersAgentesaude } from '../users/shared/users-agentesaude';
+import { Login } from './../users/shared/login';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+private user: string;
 
   constructor(
     private afa: AngularFireAuth,
+    private afs: AngularFirestore,
     private router: Router
   ) { }
 
@@ -22,5 +28,46 @@ export class AuthService {
     this.afa.signOut();
     this.router.navigate(['/login']);
   }
+
+  registerPaciente(user: UsersPaciente){
+    this.afa.createUserWithEmailAndPassword(user.email, user.password);
+
+    // this.afa.onAuthStateChanged((users)=>{
+    //   users.updateProfile({displayName: user.name, photoURL: ''});
+    //   if(users){
+    //     this.registerUserPaciente(user, users.uid);
+    //   }
+    // })
+  }
+
+  registerUserPaciente(user: UsersPaciente, id: string){
+    const { tipousuario,
+      name,
+      email } = user;
+    this.afs.collection('users').doc(id).set(
+      {
+        name: name,
+        email: email,
+        
+      }
+    )
+  }
+
+  registerAgente(user: UsersAgentesaude){
+    this.afa.createUserWithEmailAndPassword(user.email, user.password);
+
+    this.afa.onAuthStateChanged((userProfile)=>{
+      userProfile.updateProfile({displayName: user.name, photoURL: ''});
+      if(userProfile){
+        this.registerAgentesaude(user, userProfile.uid);
+      }
+    })
+  }
+
+  registerAgentesaude(user: UsersAgentesaude, id: string){
+    
+}
+
+
 
 }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,28 @@
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-signup',
+//   templateUrl: './signup.page.html',
+//   styleUrls: ['./signup.page.scss'],
+// })
+// export class SignupPage implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { UsersAgentesaude } from './../users/shared/users-agentesaude';
+import { UsersPaciente } from './../users/shared/users-paciente';
+import { ToastService } from '../shared/toast.service';
+import { AuthService } from '../shared/auth.service';
+
+
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +30,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  usersPaciente: UsersPaciente;
+  usersAgentesaude: UsersAgentesaude;
+  tipousuario: string;
+  email: string;
+  password: string;
+  // email: string;
+  // password: string;
+  // user: string;
+  // professionals: any[] = [];
 
-  constructor() { }
+  //professionalDescription: string;
+
+
+  constructor(private auth:AuthService,
+       private toast: ToastService,
+    private router: Router) 
+  { }
 
   ngOnInit() {
+    this.usersPaciente = new UsersPaciente();
+    this.usersAgentesaude = new UsersAgentesaude();
+    //this.professionalService.getAll().subscribe( (data:any) => {
+      //this.professionals = data;
+    }
+    //)
+    
+  
+
+
+  onChange(){
+    console.log(this.tipousuario)
   }
 
+
+
+  async registerPaciente(){
+    this.usersPaciente.email = this.email;
+    this.usersPaciente.password = this.password;
+    this.usersPaciente.tipousuario = this.tipousuario;
+
+    try {
+      await this.auth.registerPaciente(this.usersPaciente);
+      this.toast.showMessageTop('Usuário registrado com sucesso !!!', 'secondary');
+      this.router.navigate(['login']);
+    } catch (error) {
+      this.toast.showMessageTop(error,'danger');
+    }
+  }
+
+registerAgentesaude(){
+
+
 }
+
+
+}
+  // setProfessional(professional:any){
+  //     if(professional){
+  //       // console.log(professional);
+  //       this.usersAgentesaude.professional = this.professionals.find(item => item.id === this.usersAgentesaude.id_professional).description
+  //     } else {
+  //       this.usersAgentesaude.professional = "";
+  //     }
+  // }
+
